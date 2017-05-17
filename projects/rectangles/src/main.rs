9637094de7bdb22397c11844e23932e4a79ffbf0
kaikai1024 <kaikaifeng1024@gmail.com>
2017-05-17 15:44:29 +0800

    example

---
+++ projects/rectangles/src/main.rs
@@ -0,0 +1,20 @@
+#[derive(Debug)]
+
+struct Rectangle {
+    length: u32,
+    width: u32,
+}
+
+fn main() {
+    let rect1 = Rectangle { length: 50, width: 30};
+    println!("rect1 is {:#?}", rect1);
+    println!(
+        "the area of the rectangle is {} square pixels",
+        area(&rect1)
+    );
+    
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.length * rectangle.width
+}
