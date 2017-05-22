3df44d881b958a2cd411f242631979965f1b58bd
kaikai1024 <kaikaifeng1024@gmail.com>
2017-05-22 16:52:41 +0800

    add lib.rs

--- projects/greprs/src/main.rs
+++ projects/greprs/src/main.rs
@@ -1,37 +1,26 @@
+extern crate greprs;
+
 use std::env;
-use std::fs::File;
-use std::io::prelude::*;
+use std::process;
+
+use greprs::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+                println!("problem parsing arguments: {}", err);
+                process::exit(1);
+            });
 
     println!("searching for {}", config.query);
     println!("in file {}", config.filename);
 
-    let mut f = File::open(config.filename).expect("file not found");
+    if let Err(e) = greprs::run(config) {
+        println!("application error: {}", e) ;
 
-    let mut contents = String::new();
-    f.read_to_string(&mut contents).expect("something went wrong reading the file");
-
-    println!("with text:\n{}", contents);
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Config {
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Config {
-            query: query,
-            filename: filename,
-        }
+        process::exit(1);
     }
+
 }
 
