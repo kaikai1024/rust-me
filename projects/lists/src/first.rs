2766e4d295d19eee32867e0e0d3b03b970c5121f
kaikai1024 <kaikaifeng1024@gmail.com>
2017-06-07 10:12:42 +0800

    a bad stack

--- projects/lists/src/first.rs
+++ projects/lists/src/first.rs
@@ -40,3 +40,45 @@ impl List {
     }
 
 }
+
+impl Drop for List {
+    fn drop(&mut self) {
+        let mut cur_link = mem::replace(&mut self.head, Link:: Empty);
+        while let Link::More(mut boxed_node) = cur_link {
+            cur_link = mem::replace(&mut boxed_node.next, Link::Empty);
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::List;
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+
+        // check empty list behaves right
+        assert_eq!(list.pop(),None);
+        
+        // populate list
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        // check normal removal
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+
+        // push some more just to make sure nothing's corrupted
+        list.push(4);
+        list.push(5);
+
+        // check normal removal
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), Some(4));
+
+        // check exhaustion
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
+    }
+}
